edc351f130724db1dd7de1d6e4821678a5e1b122
einsol <Artem.Bytko@ruhr-uni-bochum.de>
2023-04-14 10:18:26 +0200

    add alert pop up

---
+++ components/alert.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const Alert = ({ message, duration = 3000 }) => {
+    const [showAlert, setShowAlert] = useState(true);
+
+    useEffect(() => {
+        setShowAlert(true);
+        const timer = setTimeout(() => {
+            setShowAlert(false);
+        }, duration);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [duration, message]);
+
+    return (
+        <div
+            className={`fixed top-0 left-0 right-0 mt-4 mx-auto w-11/12 md:w-1/2 lg:w-1/3 ${
+                showAlert ? 'transform translateY-0 opacity-100' : 'transform -translate-y-full opacity-0'
+            } transition-all duration-500 ease-in-out z-50`}
+        >
+            <div
+                className='rounded-md p-4 text-white bg-red-500'
+            >
+                {message}
+            </div>
+        </div>
+    );
+};
+
+export default Alert;
